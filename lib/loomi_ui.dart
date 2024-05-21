6a07d39c49124cf78bb24bea8456d97e238a1c33
Matheus Felipe <mfasmatheus@gmail.com>
2024-05-21 20:31:53 -0300

    [UPDATE] Create export file

---
+++ lib/loomi_ui.dart
@@ -0,0 +1,14 @@
+library loomi_ui;
+
+export 'src/theme/app_colors.dart';
+export 'src/theme/app_fonts.dart';
+export 'src/theme/app_theme.dart';
+export 'src/widgets/home/bonus_card.dart';
+export 'src/widgets/home/championship_card.dart';
+export 'src/widgets/home/date_viewer.dart';
+export 'src/widgets/home/home_tab_bar.dart';
+export 'src/widgets/home/info_card.dart';
+export 'src/widgets/home/match_summary/match_summary_card.dart';
+export 'src/widgets/home/section_header.dart';
+export 'src/widgets/home/text_button_row.dart';
+export 'src/widgets/home/tip_card.dart';
