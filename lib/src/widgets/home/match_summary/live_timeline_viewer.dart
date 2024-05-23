e3672727b4bdf946537a4d4561d36956336bbd08
Matheus Felipe <mfasmatheus@gmail.com>
2024-05-23 17:00:17 -0300

    [UPDATE] Refactor live timeline viewer

--- lib/src/widgets/home/match_summary/live_timeline_viewer.dart
+++ lib/src/widgets/home/match_summary/live_timeline_viewer.dart
@@ -13,43 +13,47 @@ class LiveTimelineViewer extends StatelessWidget {
         children: [
           Text('0', style: AppFonts.montserratMedium(12, AppColors.darkGrey)),
           const SizedBox(width: 8),
-          const Expanded(
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                LinearProgressIndicator(
-                  value: 0.6,
-                  color: AppColors.yellow,
-                ),
-                Positioned(
-                  top: -10,
-                  left: 40,
-                  child: EventTimelineIndicator(
-                    eventIconPath: "assets/images/ic_yellow_card.png",
+          Expanded(
+            child: LayoutBuilder(builder: (_, constraints) {
+              final double progressWidth = constraints.maxWidth * 0.3;
+
+              return Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  const LinearProgressIndicator(
+                    value: 0.6,
                     color: AppColors.yellow,
-                    isArrowUp: false,
                   ),
-                ),
-                Positioned(
-                  top: -15,
-                  left: 80,
-                  child: EventTimelineIndicator(
-                    eventIconPath: "assets/images/ic_red_card.png",
-                    color: AppColors.red,
-                    isArrowUp: true,
+                  Positioned(
+                    top: -10,
+                    left: progressWidth - 40,
+                    child: const EventTimelineIndicator(
+                      eventIconPath: "assets/images/ic_yellow_card.png",
+                      color: AppColors.yellow,
+                      isArrowUp: false,
+                    ),
                   ),
-                ),
-                Positioned(
-                  top: -15,
-                  right: 100,
-                  child: EventTimelineIndicator(
-                    eventIconPath: "assets/images/ic_soccer.png",
-                    color: AppColors.grey,
-                    isArrowUp: true,
+                  Positioned(
+                    top: -15,
+                    left: progressWidth - 2,
+                    child: const EventTimelineIndicator(
+                      eventIconPath: "assets/images/ic_red_card.png",
+                      color: AppColors.red,
+                      isArrowUp: true,
+                    ),
                   ),
-                ),
-              ],
-            ),
+                  Positioned(
+                    top: -15,
+                    right: progressWidth,
+                    child: const EventTimelineIndicator(
+                      eventIconPath: "assets/images/ic_soccer.png",
+                      color: AppColors.grey,
+                      isArrowUp: true,
+                    ),
+                  ),
+                ],
+              );
+            }),
           ),
           const SizedBox(width: 8),
           Text('90', style: AppFonts.montserratMedium(12, AppColors.darkGrey)),
